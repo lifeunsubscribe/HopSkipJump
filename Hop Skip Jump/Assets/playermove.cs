2b4f1c5ca43ba58bbb0e9f7f3c04922718bad605
Sarah <smw16c@my.fsu.edu>
2020-02-04 15:55:55 -0500

    bare bones og upload
    basic player sprite and movement script added

---
+++ Hop Skip Jump/Assets/playermove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * speed);
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+    }
+}
