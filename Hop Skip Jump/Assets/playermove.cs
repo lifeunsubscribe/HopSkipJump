24ad98a8f4be796d0bd4af94a862614848e7aa06
Sarah <smw16c@my.fsu.edu>
2020-02-12 14:28:41 -0500

    added bounds script documentation
    updated documentation to instruct reader how to add script that creates boundaries that prevent the player from leaving the screen

--- Hop Skip Jump/Assets/playermove.cs
+++ Hop Skip Jump/Assets/playermove.cs
@@ -6,25 +6,48 @@ public class playermove : MonoBehaviour
 {
     [SerializeField]
     private float speed;
-
     private Vector2 direction;
+    public static int HSJ;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        HSJ = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetInput();
+        //if(HSJ == 1)
+            GetInput();
         Move();
+
+        if (HSJ > 2)
+            HSJ = 0;
+        else
+            HSJ++;
     }
 
     private void Move()
     {
         transform.Translate(direction * speed);
+    }
+
+    private bool Step()
+    {
+        if (HSJ == 1)
+        {
+            HSJ++;
+            return true;
+        }   
+        else
+        {
+            if(HSJ > 2)
+                HSJ = 1;
+            HSJ++;
+            return false;
+        }
+            
     }
 
     private void GetInput()
@@ -34,18 +57,22 @@ public class playermove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             direction += Vector2.up;
+            //Step();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             direction += Vector2.down;
+            //Step();
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             direction += Vector2.left;
+            //Step();
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             direction += Vector2.right;
+            //Step();
         }
     }
 }
